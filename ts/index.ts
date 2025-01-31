e836c7e339e660c80834eb7e8803fd5bf50d23f0
flaviare1s <flaviarster@gmail.com>
2025-01-31 05:43:38 -0300

    Instala o Webpack e ajusta as configurações.

--- ts/index.ts
+++ ts/index.ts
@@ -1,5 +1,9 @@
+import { apiKey } from "../utils/apiKey";
+
 const form = document.querySelector("#search-form > form");
-const input: HTMLInputElement | null = document.querySelector("#input-localization");
+const input: HTMLInputElement | null = document.querySelector(
+  "#input-localization"
+);
 const sectionWeatherInfo = document.querySelector("#weather-info");
 
 form?.addEventListener("submit", async (event) => {
@@ -12,20 +16,20 @@ form?.addEventListener("submit", async (event) => {
     alert("O local precisa ter, pelo menos, 3 letras!");
     return;
   }
-  
+
   try {
     const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${localization}&appid=YOUR_API_KEY_HERE&units=metric&lang=pt_br`
-  );
-  const data = await response.json();
+      `https://api.openweathermap.org/data/2.5/weather?q=${localization}&appid=${apiKey}&units=metric&lang=pt_br`
+    );
+    const data = await response.json();
 
-  const infos = {
-    temp: Math.round(data.main.temp),
-    localization: data.name,
-    icon: `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`,
-  }
+    const infos = {
+      temp: Math.round(data.main.temp),
+      localization: data.name,
+      icon: `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`,
+    };
 
-  sectionWeatherInfo.innerHTML = `
+    sectionWeatherInfo.innerHTML = `
     <div class="weather-data">
         <h2>${infos.localization}</h2>
         <span>${infos.temp}°C</span>
